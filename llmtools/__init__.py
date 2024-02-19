2c6971b12990aabc643a1a4af7e75c08e3f26f92
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2024-02-19 22:57:59 +0000

    Setup

---
+++ llmtools/__init__.py
@@ -0,0 +1,5 @@
+
+from llmtools.setup import logging as log
+from llmtools.setup import set_openai_key, set_huggingface_key, setup_logging
+from llmtools.lc_docfunctions import *
+from llmtools.lc_cache import setup_inmemory_cache, setup_sqlite_cache
