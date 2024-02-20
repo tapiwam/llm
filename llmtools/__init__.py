61669de95b6b40c599bb966e3277a7808d3eac24
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2024-02-20 12:33:53 -0600

    Complete crag example

--- llmtools/__init__.py
+++ llmtools/__init__.py
@@ -3,3 +3,4 @@ from llmtools.setup import logging as log
 from llmtools.setup import set_openai_key, set_huggingface_key, setup_logging
 from llmtools.lc_docfunctions import *
 from llmtools.lc_cache import setup_inmemory_cache, setup_sqlite_cache
+from llmtools.rag.langgraph_crag import LangGraphContextualRag, ChromaDbWebLoader
